fbfb4867f0dc12a4b01cfe2c0b49ec1f53a44543
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-21 08:19:07 +0300

    feat: restructure app layout with aside-category-list component and enhance navbar and footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,9 +5,10 @@ import { featherAirplay ,featherPlusCircle} from '@ng-icons/feather-icons';
 import { heroUsers ,heroCommandLine} from '@ng-icons/heroicons/outline';
 import { Navbar } from "./shared/components/navbar/navbar";
 import { Footer } from "./shared/components/footer/footer";
+import { AsideCategoryList } from "./shared/components/aside-category-list/aside-category-list";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgIcon, Navbar, Footer],
+  imports: [RouterOutlet, NgIcon, Navbar, Footer, AsideCategoryList],
   viewProviders: [provideIcons({ featherAirplay, featherPlusCircle, heroUsers ,heroCommandLine }),],
   templateUrl: './app.html',
   styleUrl: './app.css'
