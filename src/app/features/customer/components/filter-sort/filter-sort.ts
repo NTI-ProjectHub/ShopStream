43ba4b6d7eed2359ba76876938629a7bdf5a395b
Fady Alamir <fadyalamer72@gmail.com>
2025-08-22 01:27:17 +0300

    make filter-sort & promo-cards

--- src/app/features/customer/components/filter-sort/filter-sort.ts
+++ src/app/features/customer/components/filter-sort/filter-sort.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from "@ng-icons/core";
+import { heroChevronDown, heroStar, heroTag } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-filter-sort',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './filter-sort.html',
-  styleUrl: './filter-sort.css'
+  styleUrl: './filter-sort.css',
+  providers: [
+    provideIcons({ heroStar, heroTag, heroChevronDown })
+  ]
 })
 export class FilterSort {
+  filters = [
+    'Offers',
+    'Price',
+    'Delivery fee',
+    'Highest rated',
+    'Rating',
+    'Dietary',
+    'Sort'
+  ];
 
+  dropdowns: Record<string, string[]> = {
+    'Price': ['Cheap', 'Moderate', 'Expensive'],
+    'Dietary': ['Vegetarian', 'Vegan', 'Halal', 'Gluten-free'],
+    'Sort': ['Highest rated', 'Lowest price', 'Fastest delivery']
+  };
+
+  openDropdown: string | null = null;
+
+  toggleDropdown(filter: string) {
+    this.openDropdown = this.openDropdown === filter ? null : filter;
+  }
+
+    closeDropdown() {
+    this.openDropdown = null;
+  }
 }
--- src/app/features/customer/components/promo-cards/promo-cards.ts
+++ src/app/features/customer/components/promo-cards/promo-cards.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-promo-cards',
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './promo-cards.css'
 })
 export class PromoCards {
+  promos = [
+    { title: 'Nordsee', desc: '20% Rabatt auf Klassiker', image: 'promo-cards/imgi_87.webp' },
+    { title: 'Zenzei Sushi', desc: 'Tolle Deals und faire Preise', image: 'promo-cards/imgi_88.webp' },
+    { title: 'Korean BBQ', desc: 'Jetzt gratis Lieferung sichern', image: 'promo-cards/imgi_89.webp' },
+    { title: 'Pizza Hut', desc: 'Knusprige Pizzen zum Angebot', image: 'promo-cards/imgi_90.webp' },
+  ];
 
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
+  showLeftArrow = false;
+  showRightArrow = true;
+
+  ngAfterViewInit() {
+    this.checkScroll();
+  }
+
+  scrollLeft() {
+    this.scrollContainer.nativeElement.scrollBy({ left: -1000, behavior: 'smooth' });
+    this.checkScroll();
+  }
+
+  scrollRight() {
+    this.scrollContainer.nativeElement.scrollBy({ left: 1000, behavior: 'smooth' });
+    this.checkScroll();
+  }
+
+  checkScroll() {
+    const el = this.scrollContainer.nativeElement;
+    this.showLeftArrow = el.scrollLeft > 0;
+    this.showRightArrow = el.scrollLeft + el.clientWidth < el.scrollWidth;
+  }
 }
