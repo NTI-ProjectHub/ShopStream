d0af19bac4d6fc22d9ea341b9cc1f68f56371ada
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-21 14:40:07 +0300

    feat: update navbar component with search functionality and improve layout structure

--- src/app/layout/components/navbar/navbar.ts
+++ src/app/layout/components/navbar/navbar.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { featherMenu } from '@ng-icons/feather-icons';
+import { lucideShoppingCart } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgIcon, FormsModule], // Add FormsModule here
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  styleUrl: './navbar.css',
+  viewProviders: [provideIcons({ featherMenu, lucideShoppingCart })]
 })
 export class Navbar {
-
+  location = 'Berlin';
+  time = 'Now';
+  searchQuery: string = '';
 }
