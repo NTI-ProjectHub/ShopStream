f1e2229c586eba7a6a0c823fff4c533bef193b2d
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-21 09:16:05 +0300

    feat: refactor layout components and implement aside-category-list with new structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,9 @@ import { RouterOutlet } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { featherAirplay ,featherPlusCircle} from '@ng-icons/feather-icons';
 import { heroUsers ,heroCommandLine} from '@ng-icons/heroicons/outline';
-import { Navbar } from "./shared/components/navbar/navbar";
-import { Footer } from "./shared/components/footer/footer";
-import { AsideCategoryList } from "./shared/components/aside-category-list/aside-category-list";
+import { Navbar } from "./layout/components/navbar/navbar";
+import { Footer } from "./layout/components/footer/footer";
+import { AsideCategoryList } from "./layout/components/aside-category-list/aside-category-list";
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NgIcon, Navbar, Footer, AsideCategoryList],
--- src/app/layout/components/aside-category-list/aside-category-list.ts
+++ src/app/layout/components/aside-category-list/aside-category-list.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-aside-category-list',
-  imports: [],
+  imports: [ RouterOutlet,Footer],
   templateUrl: './aside-category-list.html',
   styleUrl: './aside-category-list.css'
 })
