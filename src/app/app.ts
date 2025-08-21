f781c8fc6b900650f7c5c950d547fd9f93f57cf3
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-21 07:55:40 +0300

    feat: implement main application structure with navbar, footer, and routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { featherAirplay ,featherPlusCircle} from '@ng-icons/feather-icons';
 import { heroUsers ,heroCommandLine} from '@ng-icons/heroicons/outline';
+import { Navbar } from "./shared/components/navbar/navbar";
+import { Footer } from "./shared/components/footer/footer";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NgIcon],
+  imports: [RouterOutlet, NgIcon, Navbar, Footer],
   viewProviders: [provideIcons({ featherAirplay, featherPlusCircle, heroUsers ,heroCommandLine }),],
-
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
