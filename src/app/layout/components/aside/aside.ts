83929a32167fbb823e86bbd2c28f2ddfd7da936d
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-24 17:44:12 +0300

    Refactor aside and navbar components: remove old code, implement new structure and styles for improved user experience

--- src/app/layout/components/aside/aside.ts
+++ src/app/layout/components/aside/aside.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  lucideHouse,
+  lucideApple,
+  lucideStore,
+  lucideWine,
+  lucidePill,
+  lucideShoppingBasket,
+  lucidePawPrint,
+  lucideLeaf,
+  lucideHeart,
+  lucideCircuitBoard,
+  lucideUsers,
+  lucideWallet,
+  lucideLifeBuoy,
+  lucideCar,
+  lucideTicket,
+  lucideGift,
+  lucideLogOut,
+  lucideBuilding2
+} from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-aside',
-  imports: [],
+  standalone: true,
+  imports: [NgIcon],
   templateUrl: './aside.html',
-  styleUrl: './aside.css'
+  styleUrls: ['./aside.css'],
+  viewProviders: [provideIcons({
+    lucideHouse,
+    lucideApple,
+    lucideStore,
+    lucideWine,
+    lucidePill,
+    lucideShoppingBasket,
+    lucidePawPrint,
+    lucideLeaf,
+    lucideHeart,
+    lucideCircuitBoard,
+    lucideUsers,
+    lucideWallet,
+    lucideLifeBuoy,
+    lucideCar,
+    lucideTicket,
+    lucideGift,
+    lucideLogOut,
+    lucideBuilding2
+  })]
 })
 export class Aside {
+  isOpen = true;
+
+  toggleAside() {
+    this.isOpen = !this.isOpen;
+  }
+
+
+closeAside() {
+  this.isOpen = false;
+}
 
 }
--- src/app/layout/components/navbar/navbar.ts
+++ src/app/layout/components/navbar/navbar.ts
@@ -3,10 +3,11 @@ import { NgIcon, provideIcons } from '@ng-icons/core';
 import { FormsModule } from '@angular/forms';
 import { featherMenu, featherSearch, featherChevronDown } from '@ng-icons/feather-icons';
 import { lucideShoppingCart } from '@ng-icons/lucide';
+import { Aside } from '../aside/aside'; // استدعاء الكمبونينت
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgIcon, FormsModule],
+  imports: [NgIcon, FormsModule, Aside],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
   viewProviders: [provideIcons({ featherMenu, lucideShoppingCart, featherSearch, featherChevronDown })]
@@ -15,8 +16,9 @@ export class Navbar {
   location = 'Berlin';
   time = 'Now';
   searchQuery: string = '';
+  showAside = false;
 
   appear() {
-    console.log('Menu clicked!');
+    this.showAside = !this.showAside;
   }
 }
