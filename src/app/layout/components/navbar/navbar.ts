082dd619072fb1716407202394eed13c78f900a7
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-24 17:43:53 +0300

    Refactor footer and navbar components: remove old code, add new structure and styles for Uber Eats branding

--- src/app/layout/components/navbar/navbar.ts
+++ src/app/layout/components/navbar/navbar.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
-import { featherMenu } from '@ng-icons/feather-icons';
+import { FormsModule } from '@angular/forms';
+import { featherMenu, featherSearch, featherChevronDown } from '@ng-icons/feather-icons';
 import { lucideShoppingCart } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgIcon, FormsModule], // Add FormsModule here
+  imports: [NgIcon, FormsModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
-  viewProviders: [provideIcons({ featherMenu, lucideShoppingCart })]
+  viewProviders: [provideIcons({ featherMenu, lucideShoppingCart, featherSearch, featherChevronDown })]
 })
 export class Navbar {
   location = 'Berlin';
   time = 'Now';
   searchQuery: string = '';
+
+  appear() {
+    console.log('Menu clicked!');
+  }
 }
